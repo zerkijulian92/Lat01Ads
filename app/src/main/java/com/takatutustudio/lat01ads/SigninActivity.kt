847ad33272433fe91d7823d421e3f67f089ae522
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-04 00:02:15 +0700

    HOME (Bagian 7) - 3. Membuat Design Sign In Page

---
+++ app/src/main/java/com/takatutustudio/lat01ads/SigninActivity.kt
@@ -0,0 +1,12 @@
+package com.takatutustudio.lat01ads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
