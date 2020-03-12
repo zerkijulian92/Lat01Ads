f01e891ce533e904bc2635314492423887790ba9
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-12 21:30:17 +0700

    (BAGIAN 7) - 8. Menambahkan Efect Motion Layout

--- app/src/main/java/com/takatutustudio/lat01ads/WelcomeActivity.kt
+++ app/src/main/java/com/takatutustudio/lat01ads/WelcomeActivity.kt
@@ -10,7 +10,7 @@ class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_welcome_start)
 
         // Berpindah Activity
         btn_getstarted.setOnClickListener {
